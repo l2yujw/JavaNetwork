4e6e60b09fa232e63911b384cd69bab1ff40a792
ryu <jeungwon28@gmail.com>
2023-12-09 00:38:20 +0900

    ADD: Thread 방식

---
+++ Thread/ThreadCreation/Runnable/Main.java
@@ -0,0 +1,14 @@
+package ThreadCreation.Runnable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread a1 = new Thread(new CountDown(2));
+        Thread b1 = new Thread(new CountDown(2));
+        Thread c1 = new Thread(new CountDown(2));
+        a1.start();
+        b1.start();
+        c1.start();
+    }
+
+}
---
+++ Thread/ThreadCreation/ThreadEX/ThreadEx01.java
@@ -0,0 +1,20 @@
+package ThreadCreation.ThreadEX;
+
+public class ThreadEx01 extends Thread {
+
+    public ThreadEx01(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 10; i > -1; i--) {
+            System.out.println(getName() + ": " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
