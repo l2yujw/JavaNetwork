ed081d9a0b7814bd56549c4a173e4ce25fd1f830
ryu <jeungwon28@gmail.com>
2023-12-09 00:39:38 +0900

    ADD: Daemon Thread 동작

---
+++ Thread/Daemon/Notification.java
@@ -0,0 +1,17 @@
+package com.gmail.juyonglee0208.daemon;
+
+public class Notification implements Runnable {
+
+    @Override
+    public void run() {
+        while (true) {
+            int message = (int) (Math.random() * 10) + 1;
+            try {
+                System.out.println("Message Count: " + message);
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
